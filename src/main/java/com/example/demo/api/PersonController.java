43382588dd28384189b7e683d950b21a1fe55a51
Saeed Khan <saeed@skdevelopment.org>
2022-04-01 18:12:23 -0600

    added normal java classes

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,17 @@
+package com.example.demo.api;
+
+import com.example.demo.service.PersonService;
+import com.example.demo.model.Person;
+
+
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
+    public void addPerson(Person person) {
+        personService.addPerson(person);
+    }
+}
