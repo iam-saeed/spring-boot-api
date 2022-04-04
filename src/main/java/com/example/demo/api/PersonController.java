4726ceaff36b576b94607e36e0f719bbb113ff9b
Saeed Khan <saeed@skdevelopment.org>
2022-04-04 11:00:33 -0600

    added put and delete methods, and successfully tested on postman

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,9 +3,11 @@ package com.example.demo.api;
 import com.example.demo.service.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,14 @@ public class PersonController {
     @GetMapping(path = "{id}")
     public Person getPersonById(@PathVariable("id") UUID id) {
         return personService.getPersonById(id).orElse(null);
+    }
+    @DeleteMapping(path="{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path="{id}")
+    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
     }
 }
--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,7 +5,7 @@ import com.example.demo.model.Person;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
-import java.util.Collection.stream;
+
 
 
 @Repository("fakeDao")
@@ -33,12 +33,25 @@ public class FakePersonDataAccessService implements PersonDao{
     @Override
     public int deletePersonById(UUID id) {
         // TODO Auto-generated method stub
-        return 0;
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()) {
+            return 0;
+        } else {
+            DB.remove(personMaybe.get());
+            return 1;
+        }
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
+    public int updatePersonById(UUID id, Person update) {
         // TODO Auto-generated method stub
-        return 0;
+        return selectPersonById(id).map(person -> {
+            int indexOfPersonToUpdate = DB.indexOf(person);
+            if (indexOfPersonToUpdate >= 0) {
+                DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
+                return 1;
+            }
+            return 0;
+        }).orElse(0);
     }
 }
